24f68886a4ba3586dc7c7dc4f0dcb0dbf1e72b3f
murat beseoglu <mbeseoglu@gmail.com>
2026-03-10 00:49:38 +0000

    ikon tam olmadı tam neflix iconu koy ve kırmızı olsun (_for element path

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,13 @@
+
 "use client"
 
 import Link from 'next/link';
 import { Accessibility, Facebook, Twitter, Instagram, Youtube, Mail, Heart, Medal } from 'lucide-react';
+import { cn } from '@/lib/utils';
 
 const NetflixIcon = (props: React.SVGProps<SVGSVGElement>) => (
   <svg viewBox="0 0 24 24" fill="currentColor" {...props}>
-    <path d="M4.64 0V24C5.88 23.31 7.15 22.51 8.35 21.6V0H4.64ZM15.65 0V21.6C16.85 22.51 18.12 23.31 19.36 24V0H15.65ZM8.35 0L15.65 21.6V24C14.41 23.31 13.14 22.51 11.94 21.6L4.64 0H8.35Z" />
+    <path d="M19.36 0v24c-1.24-.69-2.51-1.49-3.71-2.4V0h3.71zM4.64 0v24C5.88 23.31 7.15 22.51 8.35 21.6V0H4.64zM8.35 0l7.3 21.6V24c-1.24-.69-2.51-1.49-3.71-2.4L4.64 0h3.71z" />
   </svg>
 );
 
@@ -15,7 +17,7 @@ export function Footer() {
     { Icon: Twitter, href: "https://x.com/AvSerkanBayram" },
     { Icon: Instagram, href: "https://www.instagram.com/av.serkanbayram/" },
     { Icon: Youtube, href: "https://www.youtube.com/@av.serkanbayram3935" },
-    { Icon: NetflixIcon, href: "https://www.netflix.com/tr/title/81676907" }
+    { Icon: NetflixIcon, href: "https://www.netflix.com/tr/title/81676907", customColor: "text-[#E50914]" }
   ];
 
   return (
@@ -45,7 +47,7 @@ export function Footer() {
                   rel={social.href.startsWith('http') ? "noopener noreferrer" : undefined}
                   className="w-12 h-12 bg-white/5 rounded-2xl flex items-center justify-center hover:bg-[#ffc107] hover:text-[#0a192f] transition-all hover:-translate-y-1"
                 >
-                  <social.Icon className="w-6 h-6" />
+                  <social.Icon className={cn("w-6 h-6", social.customColor || "text-white")} />
                 </Link>
               ))}
             </div>
